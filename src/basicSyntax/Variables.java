b6301dfb91c532048b9ba1702901c5189f23f8cf
Koheiarai0401 <koheiarai0401@gmail.com>
2024-07-02 02:20:12 +0900

    変数完了しました。

---
+++ src/basicSyntax/Variables.java
@@ -0,0 +1,18 @@
+package basicSyntax;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		int  number = 25;
+		double point = 1.5;
+		
+		System.out.println(number);
+		System.out.println(point);
+				
+		System.out.println("私の好きな" + "整数は" + number + "です。");
+		System.out.println("私の好きな" + "少数は" + point +"です。");
+
+	}
+
+}
