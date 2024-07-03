6043cc39d0c1708de7da4aaf5bfcc160277d3af1
Koheiarai0401 <koheiarai0401@gmail.com>
2024-07-03 09:19:04 +0900

    繰り返し処理行いました。確認お願い致します。

---
+++ src/basicSyntax/ForWhile.java
@@ -0,0 +1,36 @@
+package basicSyntax;
+
+public class ForWhile {
+
+	public static void main(String[] args) {
+		for (int num = 1; num <= 100; num++) {
+			if (num % 15 == 0) {
+				System.out.println("FizzBizz");
+			} else if (num % 3 == 0) {
+				System.out.println("Fizz");
+			} else if (num % 5 == 0) {
+				System.out.println("Bizz");
+			}
+		}
+		
+		
+		String[] languages = {"Ruby", "Golang", "Java", "Python", "Typescript", "HTML", "Javascript"};
+		
+		for (String language: languages) {
+			if (language == "Java") {
+				System.out.println("現在学習中の言語はJavaです。");
+				continue;
+			}
+			
+			else if (language == "HTML") {
+				System.out.println("HTMLはプログラミング言語ではありません。");
+				break;
+			}
+			
+			System.out.println(language);
+		}
+		
+
+	}
+
+}
