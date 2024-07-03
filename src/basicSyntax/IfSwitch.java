313118bc93670dc649160c80668fa9e624081ac0
Kohei <108569088+Koheiarai0401@users.noreply.github.com>
2024-07-04 07:07:29 +0900

    Update IfSwitch.java

--- src/basicSyntax/IfSwitch.java
+++ src/basicSyntax/IfSwitch.java
@@ -5,9 +5,9 @@ public class IfSwitch {
 	public static void main(String[] args) {
 		int num = 10;
 		
-		if (num %2 == 0) {
+		if (num % 2 == 0) {
 			System.out.println("2の倍数です。");
-		} else if (num %3 == 0){
+		} else if (num % 3 == 0){
 			System.out.println("3の倍数です。");
 		} else {
 			System.out.println("どちらでもありません。");
@@ -18,13 +18,15 @@ public class IfSwitch {
 		switch (pref) {
 		case "北海":
 			System.out.println("北海道");
+			break;
 		case "東京":
 			System.out.println("東京都");
+			break;
 		case"京都":
 			System.out.println("京都府");
-		break;
+			break;
 		default:
-		System.out.println("当てはまりません。");
+			System.out.println("当てはまりません。");
 		}
 		
 		
