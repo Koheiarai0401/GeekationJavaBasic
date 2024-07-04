51b0add6d5b8c460082a18a50eb65145644dd8e6
Koheiarai0401 <koheiarai0401@gmail.com>
2024-07-04 19:17:16 +0900

    新しくFoewhileのブランチを作成し直しました。前回の修正内容とともにご確認よろしくお願いします。

---
+++ src/basicSyntax/Forwhile.java
@@ -0,0 +1,35 @@
+package basicSyntax;
+
+public class Forwhile {
+	public static void main(String[] args) {	
+		for (int num = 1; num <= 100; num++) {
+			if (num % 3 == 0 && num % 5 == 0) {
+				System.out.println("FizzBizz");
+			} else if (num % 3 == 0) {
+				System.out.println("Fizz");
+			} else if (num % 5 == 0) {
+				System.out.println("Bizz");
+			}
+		}
+		
+		
+		String[] languages = {"Ruby", "Golang", "Java", "Python", "Typescript", "HTML", "Javascript"};
+		
+		for (String language: languages) {
+			if (language == "Java") {
+				System.out.println("現在学習中の言語はJavaです。");
+				continue;
+			}
+			
+			else if (language == "HTML") {
+				System.out.println("HTMLはプログラミング言語ではありません。");
+				break;
+			}
+			
+			System.out.println(language);
+		}
+		
+
+	}
+
+}
